e56b19e69ddfdf5c17bc3b571ca4b41abcfaf8bb
brw.mfatima <brw.mfatima@ifood3rd.com>
2022-10-02 02:56:35 -0300

    renomeando algumas propriedades e separando logica na classe QuizFizzBuzz

--- FizzBuzz/FizzBuzz_Solucao/Program.cs
+++ FizzBuzz/FizzBuzz_Solucao/Program.cs
@@ -1,40 +1,40 @@
 ﻿
 Console.WriteLine("Digite o valor de x.");
-var x = int.Parse(Console.ReadLine()); // Não realizo verificação aqui, já que no enunciado está definido que o valor de entrada é um número inteiro entre 1 e 10000
 
-var n = FizzBuzz(x);
+// Não realizo verificação aqui, já que no enunciado está definido que o valor de entrada é um número inteiro entre 1 e 10000;
+var entryValue = int.Parse(Console.ReadLine());
+var quizFizzBuzz = new QuizFizzBuzz();
+quizFizzBuzz.ResultQuiz(entryValue);
 
-Console.Write("[");
+public class QuizFizzBuzz
+{
+    public string[] GetMatrixFizzBuzz(int value) {
+    var numberGroup = new string[value];
 
-for (var i = 0; i < x; i++) {
-    Console.Write($" {n[i]} ");
-}
-
-Console.Write("]\n");
-
-string[] FizzBuzz(int x) {
-    var n = new string[x];
-
-    for (var i = 1; i <= x; i++) {
-        var fizz = i % 3 == 0;
-        var buzz = i % 5 == 0;
-        var fizzBuzz = fizz && buzz;
-
-        if (fizzBuzz)
-            n[i - 1] = "FizzBuzz";
-        else if (fizz)
-            n[i - 1] = "Fizz";
-        else if (buzz)
-            n[i - 1] = "Buzz";
-        else
-            n[i  - 1] = i.ToString();
+        for (var i = 1; i <= value; i++) {
+            var fizz = i % 3 == 0;
+            var buzz = i % 5 == 0;
+            var fizzBuzz = fizz && buzz;
+            
+            if (fizzBuzz)
+                numberGroup[i - 1] = "FizzBuzz";
+            else if (fizz)
+                numberGroup[i - 1] = "Fizz";
+            else if (buzz)
+                numberGroup[i - 1] = "Buzz";
+            else
+                numberGroup[i  - 1] = i.ToString();
+        }
+        return numberGroup;
     }
 
-    return n;
+    public void ResultQuiz(int entryValue) {
+        var matrix = GetMatrixFizzBuzz(entryValue);
+        Console.Write("[");
+
+        for (var i = 0; i < entryValue; i++) {
+            Console.Write($" {matrix[i]} ");
+        }
+        Console.Write("]\n");
+    }
 }
-
-
-
-
-
-
