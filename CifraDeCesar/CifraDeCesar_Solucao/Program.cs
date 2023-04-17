2cb2202148241a01a114e3075b1a55713c90a137
Renan Martins <renanmartins1999@hotmail.com>
2023-04-17 06:59:26 -0300

    Implementando desafio da cifra de césar

---
+++ CifraDeCesar/CifraDeCesar_Solucao/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Digite uma mensagem: ");
+var s = Console.ReadLine();
+
+Console.WriteLine("Digite a chave: ");
+Int32.TryParse(Console.ReadLine(), out int chave);
+
+Console.WriteLine(Criptografar(s, chave));
+
+string Criptografar(string s, int chave){
+    var mensagemCriptografada = string.Empty;
+
+    foreach (var c in s)
+    {
+        if(EhCaracterEspecial(c)){
+            mensagemCriptografada += c;
+            continue;
+        }
+
+        var letraCriptograda = c + chave - 1;
+        mensagemCriptografada += ((char)letraCriptograda);
+    }
+
+    return mensagemCriptografada;
+}
+
+bool EhCaracterEspecial(char c)
+{
+    var caracteresEspeciais = "!@#$%&(*)-_+=§,/:;[]{}?. ";
+    return caracteresEspeciais.Contains(c);
+}
+
+
